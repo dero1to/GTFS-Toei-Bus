853915a40bf9fc560852cde9bdd7e2173dc2e5a5
Kazu <toumatuk@gmail.com>
2021-05-15 19:17:12 +0900

    add:MySQL　初期設定ファイル

---
+++ docker/mysql/sql/001-create-tables.sql
@@ -0,0 +1,133 @@
+
+---- 【drop】 ----
+-- DROP TABLE IF EXISTS agency;
+
+-- DROP TABLE IF EXISTS agency_jp;
+
+-- DROP TABLE IF EXISTS attributions;
+
+-- DROP TABLE IF EXISTS calendar;
+
+-- DROP TABLE IF EXISTS calendar_dates;
+
+-- DROP TABLE IF EXISTS fare_attributes;
+
+-- DROP TABLE IF EXISTS fare_rules;
+
+-- DROP TABLE IF EXISTS feed_info;
+
+-- DROP TABLE IF EXISTS office_jp;
+
+-- DROP TABLE IF EXISTS routes;
+
+-- DROP TABLE IF EXISTS shapes;
+
+-- DROP TABLE IF EXISTS stop_times;
+
+-- DROP TABLE IF EXISTS stops;
+
+-- DROP TABLE IF EXISTS trips;
+---- 【drop】 ----
+
+
+
+
+---- 【Create】 ----
+-- create table IF not exists agency(
+--     id INT(20) AUTO_INCREMENT,
+--     created_at Datetime DEFAULT NULL,
+--     updated_at Datetime DEFAULT NULL,
+--     PRIMARY KEY (id)
+-- ) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- create table IF not exists agency_jp(
+--     id INT(20) AUTO_INCREMENT,
+--     created_at Datetime DEFAULT NULL,
+--     updated_at Datetime DEFAULT NULL,
+--     PRIMARY KEY (id)
+-- ) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- create table IF not exists attributions(
+--     id INT(20) AUTO_INCREMENT,
+--     created_at Datetime DEFAULT NULL,
+--     updated_at Datetime DEFAULT NULL,
+--     PRIMARY KEY (id)
+-- ) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- create table IF not exists calendar(
+--     id INT(20) AUTO_INCREMENT,
+--     created_at Datetime DEFAULT NULL,
+--     updated_at Datetime DEFAULT NULL,
+--     PRIMARY KEY (id)
+-- ) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- create table IF not exists calendar_dates(
+--     id INT(20) AUTO_INCREMENT,
+--     created_at Datetime DEFAULT NULL,
+--     updated_at Datetime DEFAULT NULL,
+--     PRIMARY KEY (id)
+-- ) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- create table IF not exists fare_attributes(
+--     id INT(20) AUTO_INCREMENT,
+--     created_at Datetime DEFAULT NULL,
+--     updated_at Datetime DEFAULT NULL,
+--     PRIMARY KEY (id)
+-- ) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- create table IF not exists fare_rules(
+--     id INT(20) AUTO_INCREMENT,
+--     created_at Datetime DEFAULT NULL,
+--     updated_at Datetime DEFAULT NULL,
+--     PRIMARY KEY (id)
+-- ) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- create table IF not exists feed_info(
+--     id INT(20) AUTO_INCREMENT,
+--     created_at Datetime DEFAULT NULL,
+--     updated_at Datetime DEFAULT NULL,
+--     PRIMARY KEY (id)
+-- ) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- create table IF not exists office_jp(
+--     id INT(20) AUTO_INCREMENT,
+--     created_at Datetime DEFAULT NULL,
+--     updated_at Datetime DEFAULT NULL,
+--     PRIMARY KEY (id)
+-- ) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- create table IF not exists routes(
+--     id INT(20) AUTO_INCREMENT,
+--     created_at Datetime DEFAULT NULL,
+--     updated_at Datetime DEFAULT NULL,
+--     PRIMARY KEY (id)
+-- ) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- create table IF not exists shapes(
+--     id INT(20) AUTO_INCREMENT,
+--     created_at Datetime DEFAULT NULL,
+--     updated_at Datetime DEFAULT NULL,
+--     PRIMARY KEY (id)
+-- ) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- create table IF not exists stop_times(
+--     id INT(20) AUTO_INCREMENT,
+--     created_at Datetime DEFAULT NULL,
+--     updated_at Datetime DEFAULT NULL,
+--     PRIMARY KEY (id)
+-- ) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- create table IF not exists stops(
+--     id INT(20) AUTO_INCREMENT,
+--     created_at Datetime DEFAULT NULL,
+--     updated_at Datetime DEFAULT NULL,
+--     PRIMARY KEY (id)
+-- ) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- create table IF not exists trips(
+--     id INT(20) AUTO_INCREMENT,
+--     created_at Datetime DEFAULT NULL,
+--     updated_at Datetime DEFAULT NULL,
+--     PRIMARY KEY (id)
+-- ) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+---- 【Create】 ----
